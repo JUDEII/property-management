8c64b56bd36bbec19e5b1d719bca9304ed7ecb45
JUDEII <57414981+JUDEII@users.noreply.github.com>
2024-08-24 17:53:41 +0100

    implementing error handling using AOP

---
+++ src/main/java/com/judcompany/property_management/exception/ErrorModel.java
@@ -0,0 +1,12 @@
+package com.judcompany.property_management.exception;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ErrorModel {
+
+    private String code;
+    private String message;
+}
