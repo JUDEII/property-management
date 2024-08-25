dd85af6e0d68a76ca9d88bd90d08d014a491d5f8
JUDEII <57414981+JUDEII@users.noreply.github.com>
2024-08-25 18:30:17 +0100

    implementing logging, logback and sl4j

--- src/main/java/com/judcompany/property_management/controller/UserController.java
+++ src/main/java/com/judcompany/property_management/controller/UserController.java
@@ -2,6 +2,7 @@ package com.judcompany.property_management.controller;
 
 import com.judcompany.property_management.dto.UserDTO;
 import com.judcompany.property_management.service.UserService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,14 +19,14 @@ public class UserController {
     private UserService userService;
 
     @PostMapping("/register")
-    public ResponseEntity<UserDTO> saveProperty(@RequestBody UserDTO userDTO) {
+    public ResponseEntity<UserDTO> saveProperty(@Valid @RequestBody UserDTO userDTO) {
         userDTO = userService.register(userDTO);
         ResponseEntity<UserDTO> responseEntity = new ResponseEntity<>(userDTO, HttpStatus.CREATED);
         return responseEntity;
     }
 
     @PostMapping("/login")
-    public ResponseEntity<UserDTO> login(@RequestBody UserDTO userDTO) {
+    public ResponseEntity<UserDTO> login(@Valid @RequestBody UserDTO userDTO) {
         userDTO = userService.login(userDTO.getOwnerEmail(), userDTO.getPassword());
         return new ResponseEntity<>(userDTO, HttpStatus.OK);
     }
